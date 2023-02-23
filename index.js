11e966ff72dfe4b1bd6b453277ee81f6c94ac352
Arifin1i <108435895+Arifin1i@users.noreply.github.com>
2023-02-23 11:40:59 +0000

    mongo user uri

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(express.json());
 // console.log(process.env.DB_USER)
 // console.log(process.env.DB_Password)
 
+//MongoDB Connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hupk8pl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+
 
 //get
 app.get('/', (req, res) => {
